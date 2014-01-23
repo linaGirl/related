5956d6e898ea77dac1ece9c337c78fc3e3fa2583
Michael van der Weg <michael@vanderweg.ch>
2014-01-23 19:32:59 +0100

    update & insert works

--- test.js
+++ test.js
@@ -9,13 +9,26 @@
 	var orm = new ORM(project.config);
 
 	orm.on('load', function(err){
+		var role5;
 		log('orm loaded');
-
 		var start = Date.now();
-		orm.autogantt.skill().getRoles({id:1}, '*').find(function(err, users){
+/*
+		
+		orm.autogantt.skill().fetchRoles({id:1}, '*').find(function(err, users){
 			log.info('query took ' +( Date.now()-start) + ' ms ...');
 			log(err);
 			log(users, users.first(), users.getIds());
 			users[0].loadAll();
+
+			var first = users.first();
+			first.name = Math.random();
+			first.save();
+		});*/
+
+		role5 = new orm.autogantt.role({
+			name: Math.random()
+		}).save(function(err, newRole){
+			log.info('query took ' +( Date.now()-start) + ' ms ...');
+			log(err, role5);
 		});
 	});
