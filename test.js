d02537451f25e0f10c65ad255fc3fc07da6d973d
Michael van der Weg <michael@vanderweg.ch>
2014-01-29 21:35:04 +0100

    progress

--- test.js
+++ test.js
@@ -11,47 +11,70 @@
 
 	orm.on('load', function(err){
 		log('orm loaded');
-		var   role5
-			, arr = []
-		 	, start
-		 	, i = 10000;
+		setTimeout(function(){
+			log('executing');
 
-		while(i--) arr.push(1);
-//log(orm);
+			var   role5
+				, arr = []
+			 	, start
+			 	, i = 10000;
 
-		start = Date.now();
-/**
-		// insert 1000 roles
-		async.each(arr, function(input, next){
-			new orm.autogantt.role({
-				name: Math.random()
-			}).save(false, function(err){
-				if (err) log(err);
-				next();
+			while(i--) arr.push(1);
+	//log(orm);
+
+			start = Date.now();
+	/**
+			// insert 1000 roles
+			async.each(arr, function(input, next){
+				new orm.autogantt.role({
+					name: Math.random()
+				}).save(false, function(err){
+					if (err) log(err);
+					next();
+				});
+			}
+
+			, function(){
+				log.info('query took ' +( Date.now()-start) + ' ms ...');
 			});
-		}
+	*/
 
-		, function(){
-			log.info('query took ' +( Date.now()-start) + ' ms ...');
-		});
-*/
+			var query = orm.eventbox.event().limit(10).offset(6800);
+			log(query.getCategories(['id']));
+			query.fetchVenues(['name', 'address']);
+			query.fetchPerformers(['*']);
+			query.getCategories(['id']).getCategoryLocales(['name']).getLanguages().filter({language: 'de'});
+			query.fetchSales(['*']);
+
+			query.fetchHighlightType(['name']);
 		
 
+			query.find(function(err, events){
+				log.info('query took ' +( Date.now()-start) + ' ms ...');
+				log(err);
+				//log(events);
+			});		
+	/*
 
-		orm.autogantt.skill().fetchRoles(['*']).find(function(err, skills){
-			log.info('query took ' +( Date.now()-start) + ' ms ...');
-			log(err);
-			log(skills);
+			orm.autogantt.skill().fetchRoles(['*']).find(function(err, skills){
+				log.info('query took ' +( Date.now()-start) + ' ms ...');
+				log(err);
+				log(skills);
 
-			skills.forEach(function(skill){
-				//skill.roles.push(new orm.autogantt.role({name: 'pereg'+Math.random()}));
-			});
-		});
-/*
-		role5 = new orm.autogantt.role({
-			name: Math.random()
-		}).save(function(err, newRole){
-			log.info('query took ' +( Date.now()-start) + ' ms ...');
-			log(err, role5, newRole);
-		});*/
+
+
+				skills.forEach(function(skill){
+					//skill.roles.push(new orm.autogantt.role({name: 'pereg'+Math.random()}));
+				});
+			});*/
+	/*
+			role5 = new orm.autogantt.role({
+				name: Math.random()
+			}).save(function(err, newRole){
+				log.info('query took ' +( Date.now()-start) + ' ms ...');
+				log(err, role5, newRole);
+			});*/
+
+		}, 200);
+		
 	});
