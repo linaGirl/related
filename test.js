202122d30b7ad0c676b5860eff2139699b2dc056
Michael van der Weg <michael@vanderweg.ch>
2014-03-28 20:07:40 +0100

    added the «describe method»

--- test.js
+++ test.js
@@ -77,12 +77,14 @@
 return;
 */
 
+				orm.eventbox.event().describeMethods();
+
 
 				var   transaction = orm.transaction()
 				 	, query = transaction.eventbox.event(['*']).limit(10).offset(100);
 
 				
-				query.getMapping('event_media').describeMethods();
+				query.getMapping('event_media');
 
 				query.getEventLocale(['subtitle', 'description']).getLanguage().filter({language: 'de'});
 				query.getVenue(['name', 'address'], {id: 82358}).fetchMapping('venue_media', ['*']).fetchReference('id_media', ['*']).getCity(["*"])
