1a357614b6e444f513d2589fd935f02939d8e1be
Michael van der Weg <michael@joinbox.com>
2014-05-25 20:17:41 +0200

    fixed tests

--- test/postgres.js
+++ test/postgres.js
@@ -508,7 +508,7 @@
 
 		describe('[Grouping]', function(){
 			it('should work :)', function(done){
-				db.event(['id']).group('id').find(expect('[{"id":4,"title":"Changed title","startdate":"1970-01-01T00:00:00.000Z","enddate":"2014-05-13T16:53:20.000Z","canceled":null},{"id":1,"title":"Changed title","startdate":"1970-01-01T00:00:00.000Z","enddate":null,"canceled":null},{"id":3,"title":"Mapping Test","startdate":"1970-01-01T00:00:00.000Z","enddate":null,"canceled":true},{"id":2,"title":"Mapping Test","startdate":"1970-01-01T00:00:00.000Z","enddate":null,"canceled":null}]', done));
+				db.event(['id']).group('id').find(expect('[{"id":4},{"id":1},{"id":3},{"id":2}]', done));
 			});
 		});
 
