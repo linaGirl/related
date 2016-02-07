50283e6b22f5d44751414eb11a0ab5ad692a8ff9
em <michael@joinbox.com>
2016-02-07 18:52:18 +0100

    added event tests scaffolding

--- test/230-custom-model.js
+++ test/230-custom-model.js
@@ -20,11 +20,87 @@
 
             this.set('id_venue', 89);
         }
+
+
+        // 1. that is called when saving the model
+        onBeforeSave(parameters) {
+
+        }
+
+        // 2. its called before all queries are
+        // executed 
+        onBeforePrepare(parameters) {
+
+        }
+
+        // 3. called after all query items have
+        // been executed
+        onAfterPrepare(parameters) {
+
+        }
+
+        // 4. before the delete method is executed
+        onBeforeDelte(parameters) {
+
+        }
+
+        // 4. before the insert method is executed
+        onBeforeInsert(parameters) {
+
+        }
+
+        // 4. before the update method is executed
+        onBeforeUpdate(parameters) {
+
+        }
+
+        // 5. after the delete method is executed
+        onAfterDelete(parameters) {
+
+        }
+
+        // 5. after the insert method is executed
+        onAfterInsert(parameters) {
+
+        }
+
+        // 5. after the update method is executed
+        onAfterUpdate(parameters) {
+
+        }
+
+        // 6. after the model was saved
+        onAfterSave(parameters) {
+
+        }
+
+
+
+
+        // before the transaction is commited, can be called anytime 
+        // before the onAfterSave event or anytime thereafter
+        onBeforeCommit(parameters) {
+
+        }
+
+        // after the transaction is commited, can be called anytime 
+        // before the onAfterSave event or anytime thereafter
+        onAfterCommit(parameters) {
+
+        }
+
+        // after the transaction is rolled back, can be called anytime 
+        // before the onAfterSave event or anytime thereafter
+        onAfterRollback(parameters) {
+
+        }
     }
 
     
 
-    
+
+
+
     
     describe('Custom models', function() {
 
